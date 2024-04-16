e341583976279f19d3623b9ffff9d08ac8a7189a
Vladyslav Omelianenko <vladyslav.omelianenko@meest.com>
2024-04-16 23:43:04 +0300

    Added cron

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Services\RssReaderService;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', static function () {
--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Services\CsvServices;
 use Illuminate\Support\Facades\Schedule;
 use App\Services\RssReaderService;
 
 Schedule::call(static fn() => (new RssReaderService())->run())
     ->everyThirtyMinutes();
+
+Schedule::call(static fn() => (new CsvServices())->generate())
+    ->daily();
